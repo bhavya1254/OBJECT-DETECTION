39fe4a57fe6fed63f929ba397b3e33b9317267db
bhavya1254 <114231177+bhavya1254@users.noreply.github.com>
2023-08-12 18:45:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img = "";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function draw()
+{
+    image(img,0, 0, 640, 420);
+    fill("#00FF00");
+    text("Dog", 46, 75);
+    noFill();
+    stroke("#00FF00");
+    rect(30, 60, 450, 350 );
+}
